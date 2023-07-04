151c31ef8e388a395ea5dad7eaa366db5632e681
Ratnadip Bharde <ratnadipbharde@gmail.com>
2023-07-04 18:10:02 +0530

    project initialize

---
+++ src/main/java/com/example/EmployeePayroll/EmployeePayrollApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeePayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EmployeePayroll/EmployeePayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeePayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
